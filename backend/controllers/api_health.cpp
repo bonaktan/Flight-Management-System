07854cdfd8e776d2c95b39a7da048c65fe90adda
bonaktan <128207489+bonaktan@users.noreply.github.com>
2026-05-17 21:45:11 +0800

    [#24] Fixed one more bug that Gemini got angry w/ :3

--- backend/controllers/api_health.cpp
+++ backend/controllers/api_health.cpp
@@ -13,11 +13,12 @@ void health::healthcheck(
     orm::DbClientPtr dbClient = drogon::app().getDbClient("main");
     dbClient->execSqlAsync(
         "SELECT version();",
-        [ret](const drogon::orm::Result& result) { (*ret)["database"] = true; },
-        [ret](const drogon::orm::DrogonDbException& e) {
+        [ret, callback](const drogon::orm::Result& result) {
+            (*ret)["database"] = true;
+            callback(HttpResponse::newHttpJsonResponse(*ret));
+        },
+        [ret, callback](const drogon::orm::DrogonDbException& e) {
             (*ret)["database"] = false;
+            callback(HttpResponse::newHttpJsonResponse(*ret));
         });
-
-    HttpResponsePtr resp = HttpResponse::newHttpJsonResponse(*ret);
-    callback(resp);
 }
