a304c47dd108af785b29bc5583635a89db238828
bonaktan <128207489+bonaktan@users.noreply.github.com>
2026-05-17 21:31:03 +0800

    [#24] Making Gemini Happy :3

--- backend/controllers/api_health.cpp
+++ backend/controllers/api_health.cpp
@@ -7,17 +7,17 @@ void health::healthcheck(
     const HttpRequestPtr& req,
     std::function<void(const HttpResponsePtr&)>&& callback) {
     LOG_DEBUG << "Health Check done";
-    Json::Value ret;
-    ret["backend"] = true;
+    auto ret = std::make_shared<Json::Value>();
+    (*ret)["backend"] = true;
 
     orm::DbClientPtr dbClient = drogon::app().getDbClient("main");
-    try {
-        orm::Result result = dbClient->execSqlSync("SELECT version();");
-        ret["database"] = true;
-    } catch (drogon::orm::DrogonDbException& e) {
-        ret["database"] = false;
-    }
+    dbClient->execSqlAsync(
+        "SELECT version();",
+        [ret](const drogon::orm::Result& result) { (*ret)["database"] = true; },
+        [ret](const drogon::orm::DrogonDbException& e) {
+            (*ret)["database"] = false;
+        });
 
-    HttpResponsePtr resp = HttpResponse::newHttpJsonResponse(ret);
+    HttpResponsePtr resp = HttpResponse::newHttpJsonResponse(*ret);
     callback(resp);
 }
