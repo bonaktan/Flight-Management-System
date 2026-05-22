c3cfa6441ace7a235955e3f5a391cadc72971548
bonaktan <bon@bonnybonnybonaktan.xyz>
2026-05-22 23:19:24 +0800

    making gemini happi :3

--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-jq '.db_clients[0] |= (.host = env.POSTGRES_HOST | .port = (env.POSTGRES_PORT | tonumber) | .passwd = env.POSTGRES_PASSWORD | .dbname = env.POSTGRES_DB | .user = env.POSTGRES_USER)' /app/config.json > /app/config.tmp 
+
+jq '.db_clients[0] |= (
+    .host = (env.POSTGRES_HOST // .host) |
+    .port = (if env.POSTGRES_PORT then (env.POSTGRES_PORT | tonumber) else .port end)  | 
+    .passwd = (env.POSTGRES_PASSWORD // .passwd) | 
+    .dbname = (env.POSTGRES_DB // .dbname) | 
+    .user = (env.POSTGRES_USER // .user)
+)' /app/config.json > /app/config.tmp 
+
 mv /app/config.tmp /app/config.json
-/app/SkyBridgeAirways-Backend
+exec /app/SkyBridgeAirways-Backend
