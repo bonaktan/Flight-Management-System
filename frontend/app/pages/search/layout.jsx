acf143795586f631b825c5d49576f7c579f27c1b
bonaktan <bon@bonnybonnybonaktan.xyz>
2026-06-03 21:54:17 +0800

    Laid the foundations for the Middleware.

--- frontend/app/pages/search/layout.jsx
+++ frontend/app/pages/search/layout.jsx
@@ -1,5 +1,16 @@
 import { Outlet } from "react-router";
-export default function SearchLayout() {
+import { authMiddleware } from "../middleware/authMiddleware";
+import { AuthContext } from "../middleware/context";
+export const middleware = [authMiddleware];
+
+export async function loader({ context }) {
+    // User is guaranteed non-null here — requireAuth already checked.
+    const user = context.get(AuthContext);
+    return { user };
+}
+
+export default function SearchLayout({ loaderData }) {
+    console.log(loaderData);
     return (
         <div>
             <p>Search View (still tba :3)</p>
